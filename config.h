f2b81ba442459489cc79e7c82e459ab048b11236
Saša Savić <sasa.savic@pm.me>
2019-01-31 20:48:42 +0100

    Change layout names

--- config.h
+++ config.h
@@ -31,15 +31,15 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.6; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "T",      tile },    /* first entry is default */
-	{ "F",      NULL },    /* no layout function means floating behavior */
-	{ "M",      monocle },
+	{ "=",      tile },    /* first entry is default */
+	{ "~",      NULL },    /* no layout function means floating behavior */
+	{ "-",      monocle },
 };
 
 /* key definitions */
