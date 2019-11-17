c5f41e3b3ed04bfa709bf496f741e3922c9745cd
Saša Savić <sasa@sasa-savic.com>
2019-11-17 12:44:35 +0100

    Group keybinds

--- config.def.h
+++ config.def.h
@@ -69,26 +69,29 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key           function        argument */
-	{ MODKEY,                       XK_g,         incngaps,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_g,         incngaps,       {.i = -1 } },
 	{ MODKEY,                       XK_space,     spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return,    spawn,          {.v = termcmd } },
+
 	{ MODKEY,                       XK_j,         focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,         focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,         incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,         incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_g,         incngaps,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_g,         incngaps,       {.i = -1 } },
 	{ MODKEY,                       XK_h,         setmfact,       {.f = -0.01} },
 	{ MODKEY,                       XK_l,         setmfact,       {.f = +0.01} },
+
 	{ MODKEY,                       XK_Return,    zoom,           {0} },
+	{ MODKEY          ,             XK_c,         killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_f,         togglefloating, {0} },
+
 	{ MODKEY,                       XK_Tab,       view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,         killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_Tab,       view,           {.ui = ~0 } },
+
 	{ MODKEY,                       XK_t,         setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,         setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,         setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,     setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,     togglefloating, {0} },
-	{ MODKEY,                       XK_9,         view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_9,         tag,            {.ui = ~0 } },
+
 	{ MODKEY,                       XK_comma,     focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period,    focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,     tagmon,         {.i = -1 } },
